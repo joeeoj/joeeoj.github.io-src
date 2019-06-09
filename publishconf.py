e1f2309c0426a3d41b3f2c58ddb3ae96880c9179
Mike Yu <michael.yu06@gmail.com>
2019-06-09 08:59:57 -0700

    Add HTTPS and try to add Disqus

--- publishconf.py
+++ publishconf.py
@@ -11,7 +11,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = 'http://joeeoj.github.io'
+SITEURL = 'https://joeeoj.github.io'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
@@ -21,5 +21,5 @@ DELETE_OUTPUT_DIRECTORY = False
 
 # Following items are often useful when publishing
 
-#DISQUS_SITENAME = ""
+DISQUS_SITENAME = "joeeoj"
 #GOOGLE_ANALYTICS = ""
